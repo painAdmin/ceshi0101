9d1edcfdce4552f592fe0a0fb4661d0ccc772a52
Administrator <Administrator@10.30.26.71>
2018-05-23 13:56:00 +0800

    adsf

--- Test001GIt/src/com/Test1.java
+++ Test001GIt/src/com/Test1.java
@@ -7,16 +7,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		
       System.out.println("17");
-
-      System.out.println("1333333");
 	}
-
-	public static void exe(){
-		
-      System.out.println("1333333");
-	}
-	public static void test(){
-		
-	}
+	
 
 }
