27bfba3bb08011b3b443986859cc7dda9816cb2f
Administrator <Administrator@10.30.26.71>
2018-05-23 11:10:19 +0800

    asdasd

--- Test001GIt/src/com/Test1.java
+++ Test001GIt/src/com/Test1.java
@@ -1,18 +1,19 @@
 package com;
 
+import java.util.Date;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		
       System.out.println("17");
 
-
-      System.out.println("15");
-      System.out.println(16);
-      System.out.println("214");	
-      System.out.println("11");
+      System.out.println("1333333");
 
 
+	}
+	public static void test(){
+		
 	}
 
 }
