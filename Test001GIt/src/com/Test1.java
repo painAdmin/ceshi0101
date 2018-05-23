d2971bb6eafd79126414f13cb7d5672fdbd93399
Administrator <Administrator@10.30.26.71>
2018-05-23 11:06:45 +0800

    224

--- Test001GIt/src/com/Test1.java
+++ Test001GIt/src/com/Test1.java
@@ -1,14 +1,17 @@
 package com;
 
+import java.util.Date;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		
-      System.out.println("15");
-      System.out.println(16);
-      System.out.println("214");	
-      System.out.println("11");
+      System.out.println("17");
 
+
+      System.out.println("1120");
+    
+      Date date=new Date();
 
 	}
 
