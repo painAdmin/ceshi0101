fd8caa987104f8ad7125b56cc10643952d810dc6
Administrator <Administrator@10.30.26.71>
2018-05-23 10:52:14 +0800

    asdasd

--- Test001GIt/src/com/Test1.java
+++ Test001GIt/src/com/Test1.java
@@ -4,6 +4,12 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
+      System.out.println("14");
+
+		
+
+      System.out.println("23");
+
 
       System.out.println("adfasd");
       System.out.println("11");
