144fea50171a9c790baf27468ac2e17235481bbb
Administrator <Administrator@10.30.26.71>
2018-05-23 10:53:57 +0800

    13

--- Test001GIt/src/com/Test1.java
+++ Test001GIt/src/com/Test1.java
@@ -4,18 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-      System.out.println("14");
-
-		
-
-      System.out.println("23");
-
-
-      System.out.println("adfasd");
-      System.out.println("11");
-
-
-      System.out.println("ceshi");
+      System.out.println("13");
 	}
 
 }
