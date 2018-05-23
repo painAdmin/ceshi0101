54fb1cbd35f91f671b35b87dade39b91bf128f9a
Administrator <Administrator@10.30.26.71>
2018-05-23 10:45:18 +0800

    33

--- Test001GIt/src/com/Test1.java
+++ Test001GIt/src/com/Test1.java
@@ -5,8 +5,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		
 
-      System.out.println("ceshi");
+      System.out.println("adfasd");
+      System.out.println("11");
 
+
+      System.out.println("ceshi");
       System.out.println("adfasd");
       System.out.println("22");
 	}
