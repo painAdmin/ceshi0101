319634f89a59ec071652b64550cad570173200a0
Administrator <Administrator@10.30.26.71>
2018-05-23 11:03:16 +0800

    123

--- Test001GIt/src/com/Test1.java
+++ Test001GIt/src/com/Test1.java
@@ -4,10 +4,14 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
+      System.out.println("17");
+
+
       System.out.println("15");
       System.out.println(16);
-      System.out.println("24");	
-      System.out.println("13");
+      System.out.println("214");	
+      System.out.println("11");
+
 
 	}
 
